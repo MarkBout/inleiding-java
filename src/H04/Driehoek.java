e59eb208d0a19e2a05fb16df2e11e21330b28028
MarkBout <mbout.roc-dev.com>
2019-09-03 12:47:51 +0200

    Driehoek gemaakt met g.drawline

--- src/H04/Driehoek.java
+++ src/H04/Driehoek.java
@@ -7,10 +7,16 @@ public class Driehoek extends Applet {
     public void init() {
     }
 
-   /* public void paint(Graphics g) {
-        setBackground(Color.blue);
-        g.setColor(Color.yellow);
-        g.drawRect(20, 20, 100, 50);
-        g.fillRect(20, 80, 100, 50);
+    /*test voor mij om het lijnen tekenen te snappen
+    public void paint(Graphics g) {
+        g.drawLine(50, 100, 270, 50);
     }*/
+
+    //Driehoek
+    public void paint(Graphics g) {
+    g.drawLine(20 , 100, 80 , 100);
+    g.drawLine(20, 100, 40 , 50);
+    g.drawLine(80, 100, 40 , 50);
+
+    }
 }
