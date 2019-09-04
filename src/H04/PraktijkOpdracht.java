2bbd2fcd2f890d15753b6c7ffef66cf92b3319ba
MarkBout <mbout@roc-dev.com>
2019-09-04 17:24:45 +0200

    Praktijkopdracht H4 afgerond

--- src/H04/PraktijkOpdracht.java
+++ src/H04/PraktijkOpdracht.java
@@ -16,5 +16,25 @@ public class PraktijkOpdracht extends Applet {
         //Afgeronde rechthoek
         g.drawRoundRect(10, 150, 100, 50, 30, 30);
         g.drawString("Afgeronde Rechthoek", 10, 220);
+        //Gevulde ovaal
+        g.setColor(Color.magenta);
+        g.fillOval(130, 150, 100, 50);
+        g.setColor(Color.black);
+        g.drawString("Gevulde ovaal", 145, 220);
+        //Cirkel
+        g.drawOval(250, 150, 50, 50);
+        g.drawString("Cirkel", 250, 220);
+        //Gevulde rechthoek met ovaal
+        g.setColor(Color.magenta);
+        g.fillRect(130, 80, 100, 50);
+        g.setColor(Color.black);
+        g.drawOval(130, 80, 100, 50);
+        g.drawString("Gevulde rechthoek ovaal", 130, 145);
+        //Taartpunt met ovaal
+        g.drawOval(250, 80, 100, 50);
+        g.setColor(Color.magenta);
+        g.fillArc(250, 80, 100, 50, 0, 45);
+        g.setColor(Color.black);
+        g.drawString("Taartpunt met ovaal", 280, 145);
     }
 }
