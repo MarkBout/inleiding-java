66c6b6a40453fd9b5f1a77231d94f48c57a8c62f
MarkBout <mbout.roc-dev.com>
2019-09-04 10:45:10 +0200

    Staafdiagram gemaakt met namen en nummering

--- src/H04/StaafDiagram.java
+++ src/H04/StaafDiagram.java
@@ -8,7 +8,31 @@ public class StaafDiagram extends Applet {
     }
 
     public void paint(Graphics g) {
+        //verticale lijn
         g.drawLine( 50, 50, 50, 150);
-        g.drawLine(50, 150, 200, 150);
+        //horizontaale lijn
+        g.drawLine(50, 150, 250, 150);
+        //namen kinderen
+        g.drawString("Jeroen", 60 , 160);
+        g.drawString("Hans", 120, 160);
+        g.drawString("Valerie", 180, 160);
+        //nummering
+        g.drawString("0", 40 , 150);
+        g.drawString("20", 35, 130);
+        g.drawString("40", 35, 110);
+        g.drawString("60", 35, 90);
+        g.drawString("80", 35, 70);
+        g.drawString("100", 30, 50);
+        //Staafdiagramen
+            //jeroen
+        g.setColor(Color.MAGENTA);
+        g.fillRect(70, 50, 20, 100);
+            //Hans
+        g.setColor(Color.orange);
+        g.fillRect(125 , 70, 20, 80);
+            //Valerie
+        g.setColor(Color.green);
+        g.fillRect(190, 110, 20, 40);
+
     }
-}
+ }
