04c3982c4b7d2c95fd66f27420388198d6a735fd
MarkBout <mbout.roc-dev.com>
2019-09-05 14:22:04 +0200

    Opdracht 5.1 gemaakt, heb de viguren gemaakt met variabelen

--- src/H05/Variabelen.java
+++ src/H05/Variabelen.java
@@ -8,23 +8,38 @@ public class Variabelen extends Applet {
     int breedte;
     int hoogte;
 
-    @Override
     public void init() {
         opvulkleur = Color.MAGENTA;
         lijnkleur = Color.black;
         breedte = 200;
         hoogte = 100;
+
     }
 
     public void paint(Graphics g) {
-        //rechthoek
-    g.drawRect(100, 100, breedte, hoogte);
-        //afgewerkte rechthoek
-    g.drawRoundRect(100, 255, breedte, hoogte, 30, 30);
-    //opgevulde rechthoek kleur
-    g.setColor(opvulkleur);
-    g.fillRect(315, 100, breedte, hoogte);
-    g.setColor(lijnkleur);
-    g.drawOval(315, 100, breedte, hoogte);
+        //lijn
+        g.drawLine(50, 10,250, 10);
+        //Rechthoek
+        g.drawRect(50, 30, breedte, hoogte);
+        //Rechthoek met ovaal
+        g.setColor(opvulkleur);
+        g.fillRect(300, 30, breedte, hoogte);
+        g.setColor(lijnkleur);
+        g.drawOval(300, 30, breedte, hoogte);
+        //Taartpunt
+        g.setColor(lijnkleur);
+        g.drawOval(550, 30, breedte, hoogte);
+        g.setColor(opvulkleur);
+        g.fillArc(550, 30, breedte, hoogte, 0, 45);
+        //Cirkel
+        g.setColor(lijnkleur);
+        g.drawOval(550, 175, breedte, breedte);
+        //gevulde ovaal
+        g.setColor(opvulkleur);
+        g.fillOval(300, 175, breedte, hoogte);
+        //rechthoek met kanten
+        g.setColor(lijnkleur);
+        g.drawRoundRect(50, 175, breedte, hoogte,30,30);
     }
+
 }
