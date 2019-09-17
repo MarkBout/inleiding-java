23b059dddc09c1136644d301dca6913820b414f2
MarkBout <mbout.roc-dev.com>
2019-09-17 14:36:52 +0200

    Begonnen aan Opdracht 6

---
+++ src/H11/Opdracht6.java
@@ -0,0 +1,19 @@
+package H11;
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht6 extends Applet{
+
+    public void init() {
+    }
+
+
+    public void paint(Graphics g) {
+        int teller;
+        int x, y;
+        for (teller = 0; teller < 5; teller++)
+            {
+
+            }
+    }
+}
