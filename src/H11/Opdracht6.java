e279f07107b40697edf213247f6133309576af6e
MarkBout <mbout.roc-dev.com>
2019-09-18 08:43:28 +0200

    Opdracht 6 gemaakt 5 centraale circkels

--- src/H11/Opdracht6.java
+++ src/H11/Opdracht6.java
@@ -10,10 +10,18 @@ public class Opdracht6 extends Applet{
 
     public void paint(Graphics g) {
         int teller;
-        int x, y;
+        int lengte, breete, x, y;
+        x = 50;
+        y = 50;
+        lengte = 20;
+        breete = 20;
         for (teller = 0; teller < 5; teller++)
             {
-
+                g.drawOval(x,y,lengte,breete);
+                x += -5;
+                y += -5;
+                lengte += 10;
+                breete += 10;
             }
     }
 }
