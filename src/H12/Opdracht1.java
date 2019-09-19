b649559528d753ca82db7ea86fb4a4fd7d94b107
MarkBout <mbout.roc-dev.com>
2019-09-19 14:29:11 +0200

    Begonnen aan Opdracht 1

---
+++ src/H12/Opdracht1.java
@@ -0,0 +1,16 @@
+package H12;
+import java.awt.*;
+import java.applet.*;
+
+public class Opdracht1 extends Applet{
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+
+    }
+
+
+}
