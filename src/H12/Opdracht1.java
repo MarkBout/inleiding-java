0c4609f2b67c99ce487c414b85b02f56309acea0
MarkBout <mbout.roc-dev.com>
2019-09-20 13:22:55 +0200

    Opdracht 1 gemaakt array van 10 getallen gemaakt deze weergegeven op het scherm met het gemiddelde van deze getallen

--- src/H12/Opdracht1.java
+++ src/H12/Opdracht1.java
@@ -4,12 +4,26 @@ import java.applet.*;
 
 public class Opdracht1 extends Applet{
 
-    public void init() {
+    double[] cijfers = {6.5, 3, 7.2, 9, 2, 4.9, 2, 4.1, 8.2, 1.5};
+    double totaal;
+    int teller;
 
+    public void init()
+    {
+        for ( teller = 0; teller < cijfers.length; teller++)
+        {
+            totaal += cijfers[teller];
+        }
+        totaal /= teller;
     }
 
-    public void paint(Graphics g) {
-
+    public void paint(Graphics g)
+    {
+        for (int i = 0; i < cijfers.length; i++)
+        {
+            g.drawString("" + cijfers[i],50,20 * i +20);
+            g.drawString("Het gemiddelde is: " + totaal,100,20);
+        }
     }
 
 
