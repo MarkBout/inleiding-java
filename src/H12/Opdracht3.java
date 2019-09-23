5bca852ca1eeb5794408d0748dabe4cb4c904c4c
MarkBout <mbout@roc-dev.com>
2019-09-23 08:55:45 +0200

    Opdracht 10.4 afgemaakt zoekt nu schrikkeljaren en geeft 29 dagen bij februari als het een schrikkeljaar is.

--- src/H12/Opdracht3.java
+++ src/H12/Opdracht3.java
@@ -2,6 +2,7 @@ package H12;
 import java.awt.*;
 import java.applet.*;
 import java.awt.event.*;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Opdracht3 extends Applet
@@ -9,23 +10,37 @@ public class Opdracht3 extends Applet
     TextField teksttabel[];
     Button knopOK;
     int teller = 0;
+    int numsort[];
+    String[] strarray;
+    int i;
     public void init()
     {
+        //tekstvelden maken en toevoegen
         teksttabel = new TextField[5];
         for ( teller = 0; teller < teksttabel.length; teller++)
         {
             teksttabel[teller] = new TextField( 10);
             add(teksttabel[teller]);
         }
+        //knop maken en toevoegen
         knopOK = new Button("OK");
         knopOK.addActionListener(new Knopluisterraar());
         add(knopOK);
+        i = 0;
     }
 
     public class Knopluisterraar implements ActionListener
     {
-        public void actionPerformed(ActionEvent e) {
-            Arrays.sort(teksttabel);
+        public void actionPerformed(ActionEvent e)
+        {
+            //Strings van tekstveld naar integers converteren
+            for ( i = 0; i < numsort.length; i++)
+            {
+                    numsort[i] = Integer.parseInt(teksttabel[teller].getText());
+            }
+            Arrays.sort(numsort);
+            strarray[i] = Integer.toString(numsort[i]);
+            teksttabel[teller].setText(strarray[i]);
         }
     }
 }
