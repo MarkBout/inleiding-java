61f217a48664afd7ce173e601ff501cfa27f814f
MarkBout <mbout.roc-dev.com>
2019-09-26 12:35:07 +0200

    Praktijkopdracht H12 gebruiker kan namen en nummers invoergen

--- src/H12/PraktijkopdrachtH12.java
+++ src/H12/PraktijkopdrachtH12.java
@@ -5,36 +5,62 @@ import java.awt.event.*;
 import java.util.Arrays;
 
 public class PraktijkopdrachtH12 extends Applet{
-    TextField[] namen;
-    TextField[] nummers;
-    Button nextknop;
+    TextField nameninput,nummers;
+    boolean genoeg;
+    Button okKnop;
     int aantal;
+    String[] gebruikersnamen;
     public void init() {
-        //tekstvelden toevoegen
-        nummers = new TextField[10];
-        namen = new TextField[10];
-        for (int i = 0; i < namen.length; i++) {
-          namen[i] = new TextField("namen",20);
-          add(namen[i]);
-        }
-        for (int j = 0; j < nummers.length; j++) {
-            nummers[j] = new TextField("telnummers",20);
-        }
-        nextknop = new Button("Volgende");
-        nextknop.addActionListener(new Knoplistener());
+        setSize(800,800);
+        nameninput = new TextField("naam", 20);
+        nummers = new TextField("Telnummer",30);
+        genoeg = false;
+        add(nameninput);
+        add(nummers);
+        okKnop = new Button("Ok");
+        okKnop.addActionListener(new Knoplistener());
+        add(okKnop);
+        gebruikersnamen = new String[10];
+
+        /* NullPointerException Fix */
+        Arrays.fill(gebruikersnamen, "");
     }
+
+    public void paint(Graphics g) {
+        int y = 50;
+        for (String namen : gebruikersnamen)
+        {
+            g.drawString(namen, 60, y+=15);
+        }
+    }
+
     public class Knoplistener implements ActionListener
     {
+        String snamen, snummers;
         public void actionPerformed(ActionEvent e)
         {
-
-            aantal++;
-            if (aantal == 10)
+            //nummers en namen uit de tekstvakken halen
+            snamen = nameninput.getText().toLowerCase();
+            snummers = nummers.getText();
+            if (aantal < gebruikersnamen.length);
             {
+                gebruikersnamen[aantal] = "Naam " + snamen + "   Tel-nummer: " + snummers;
+                aantal++;
+                System.out.println("Nummers toegevoegd "+ aantal);
 
+                if (aantal == gebruikersnamen.length)
+                {
+                    genoeg = true;
+                }
             }
+            //wanneer er genoeg (10) nummers/namen zijn toegevoegd
+            if (genoeg)
+            {
+                Arrays.sort(gebruikersnamen);
+                repaint();
+            }
+            nameninput.setText("");
+            nummers.setText("");
         }
-    }
-    public void paint(Graphics g) {
     }
 }
