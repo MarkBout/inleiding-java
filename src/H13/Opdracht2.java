903010426a5bc7fb60b22629c9e72a6f149956c0
MarkBout <mbout.roc-dev.com>
2019-09-27 10:48:59 +0200

    H13 Opdracht2 rode muur op in de applet

--- src/H13/Opdracht2.java
+++ src/H13/Opdracht2.java
@@ -5,15 +5,41 @@ import java.awt.*;
 public class Opdracht2 extends Applet
 {
     public void init() {
+        setSize(400,400);
 
     }
 
-    void muurTekenen(Graphics d, int x1, int y1, int beete, int hoogte)
+    void muurTekenen(Graphics g, int x1, int y1, int beete, int hoogte)
     {
-        d.setColor(Color.red);
-        d.fillRect(x1,y1,beete,hoogte);
-    }
-    public void paint(Graphics g) {
-        muurTekenen();
+        int steenteller;
+        boolean rij2 = false;
+        for (steenteller = 0; steenteller < 5; steenteller++)
+        {
+            g.setColor(Color.red);
+            g.fillRect(x1, y1, beete, hoogte);
+            g.setColor(Color.black);
+            g.drawRect(x1, y1, beete, hoogte);
+            x1  += beete;
+        }
+        if (steenteller == 5)
+        { rij2 = true; }
+        if (rij2)
+        {
+            for (int muurteller = 0; muurteller < 5; muurteller++) {
+                x1 = 20;
+                y1 += hoogte;
+                for (steenteller = 0; steenteller < 5; steenteller++) {
+                    g.setColor(Color.red);
+                    g.fillRect(x1, y1, beete, hoogte);
+                    g.setColor(Color.black);
+                    g.drawRect(x1, y1, beete, hoogte);
+                    x1 += beete;
+                }
+            }
+        }
+        }
+    public void paint(Graphics g)
+    {
+        muurTekenen(g,20,50,100,50);
     }
 }
